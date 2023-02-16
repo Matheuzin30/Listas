4b1485a54805d267efaf2a3ee5bfdb0eae5aa6c3
Matheuzin30 <mat860913@gmail.com>
2023-02-16 19:28:02 -0300

    Adicionar arquivos de projeto.

---
+++ Listas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Listas
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("João");
+            list.Add("Matheus");
+            list.Add("Duda");
+            list.Insert(2, "Meire");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine(list.Count);
+
+            string s1 = list.Find(x => x[0] == 'D');
+            Console.WriteLine("Primeira letra 'D': " + s1);
+
+            string s3 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s3);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Fisrt position 'A':" + pos1);
+            
+        }
+    }
+}
